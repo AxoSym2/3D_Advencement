88cda9cf24c317edbe3bf98ee0ea335469ad1aaf
이강호 <tiger04176672@gmail.com>
2026-07-02 19:37:14 +0900

    [feat] 힐팩과 도트데미지, 도트힐 기능 구현

---
+++ Assets/Script/DamageZone.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class DamageZone : MonoBehaviour
+{
+    [SerializeField] private bool _isHealZone = false;
+    [SerializeField] private int _tickAmount = 10;
+    [SerializeField] private float _tickInterval = 1f;
+
+    private Dictionary<Collider, float> _nextTickTime = new Dictionary<Collider, float>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        _nextTickTime[other] = Time.time;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!_nextTickTime.ContainsKey(other)) return;
+        if (Time.time < _nextTickTime[other]) return;
+
+        ApplyTickEffect(other);
+        _nextTickTime[other] = Time.time + _tickInterval;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (_nextTickTime.ContainsKey(other))
+        {
+            _nextTickTime.Remove(other);
+        }
+    }
+
+    private void ApplyTickEffect(Collider target)
+    {
+        Health targetHealth = target.GetComponentInParent<Health>();
+        if (targetHealth == null || targetHealth.IsDead) return;
+
+        if (_isHealZone)
+        {
+            targetHealth.Heal(_tickAmount);
+        }
+        else
+        {
+            targetHealth.TakeDamage(_tickAmount);
+        }
+    }
+}
---
+++ Assets/Script/HealItem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class HealItem : MonoBehaviour
+{
+    [SerializeField] private int _healAmount = 30;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        Health targetHealth = other.GetComponentInParent<Health>();
+        if (targetHealth == null || targetHealth.IsDead) return;
+
+        targetHealth.Heal(_healAmount);
+        Destroy(gameObject);
+    }
+}
