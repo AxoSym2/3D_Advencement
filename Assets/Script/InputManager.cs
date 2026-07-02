915c65ac2fa92a97b7d8c0dae6376aff5cafd30e
이강호 <tiger04176672@gmail.com>
2026-07-02 14:05:04 +0900

    [feat] InputManager

---
+++ Assets/Script/InputManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance { get; private set; }
+
+    [Header("Key Bindings")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode runKey = KeyCode.LeftShift;
+
+    public Vector2 MoveInput {  get; private set; }
+    public bool JumpPressed { get; private set; }
+    public bool RunHeld { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        MoveInput = new Vector2(x, y);
+
+        JumpPressed = Input.GetKeyDown(jumpKey);
+        RunHeld = Input.GetKey(runKey);
+    }
+}
