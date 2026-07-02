7381319d5d73615106b99391f01c7de31594cd22
이강호 <tiger04176672@gmail.com>
2026-07-02 15:33:18 +0900

    [feat] 플레이어 애니메이션 적용

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform Transform_Target;
+    [SerializeField] private Vector3 _offset = new Vector3(0f, 3f, -6f);
+
+    private void LateUpdate()
+    {
+        if (Transform_Target == null) return;
+        transform.position = Transform_Target.position + _offset;
+    }
+}
