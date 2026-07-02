dd4b4ab7b576b9bcc044f2d923a5e3ac9a0b8771
이강호 <tiger04176672@gmail.com>
2026-07-02 16:54:53 +0900

    [feat] 오브젝트 체력 베이스 구현

---
+++ Assets/Script/UnitHealth.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth = 100;
+
+    private Animator _animator;
+    private string Anim_Hit = "Hit";
+
+    public int CurrentHealth { get; private set; }
+
+    public int MaxHealth
+    {
+        get { return _maxHealth; }
+    }
+
+    public bool IsDead { get; private set; }
+
+    public Action<int, int> OnHealthChanged;
+
+    public Action OnDeath;
+
+    private void Awake()
+    {
+        CurrentHealth = _maxHealth;
+        _animator = GetComponent<Animator>();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (IsDead) return;
+
+        CurrentHealth = Mathf.Max(CurrentHealth - amount, 0);
+        OnHealthChanged?.Invoke(CurrentHealth, _maxHealth);
+
+        if (_animator != null)
+        {
+            _animator.SetTrigger(Anim_Hit);
+        }
+
+        if (CurrentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        if (IsDead) return;
+
+        CurrentHealth = Mathf.Min(CurrentHealth + amount, _maxHealth);
+        OnHealthChanged?.Invoke(CurrentHealth, _maxHealth);
+    }
+
+    private void Die()
+    {
+        IsDead = true;
+        OnDeath?.Invoke();
+
+        gameObject.SetActive(false);
+    }
+}
