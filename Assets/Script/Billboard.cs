f8abdde917885a7d8361482d059888f3bdd4d257
이강호 <tiger04176672@gmail.com>
2026-07-02 18:39:58 +0900

    [feat] 체력 UI 구현

---
+++ Assets/Script/Billboard.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Transform _mainCameraTransform;
+
+    private void Start()
+    {
+        if (Camera.main != null)
+        {
+            _mainCameraTransform = Camera.main.transform;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (_mainCameraTransform == null) return;
+
+        transform.rotation = _mainCameraTransform.rotation;
+    }
+}
---
+++ Assets/Script/HealthbarUI.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarUI : MonoBehaviour
+{
+    [SerializeField] private Health Health_Target;
+    [SerializeField] private Slider Slider_HealthBar;
+
+    private void OnEnable()
+    {
+        if (Health_Target == null) return;
+
+        Health_Target.OnHealthChanged += UpdateHealthBar;
+
+        UpdateHealthBar(Health_Target.CurrentHealth, Health_Target.MaxHealth);
+    }
+
+    private void OnDisable()
+    {
+        if (Health_Target == null) return;
+
+        Health_Target.OnHealthChanged -= UpdateHealthBar;
+    }
+
+    private void UpdateHealthBar(int current, int max)
+    {
+        if (Slider_HealthBar == null) return;
+
+        Slider_HealthBar.maxValue = max;
+        Slider_HealthBar.value = current;
+    }
+}
