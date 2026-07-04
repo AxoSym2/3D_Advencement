2d15ef9af4a653a958f7aee717b572a55e8cc7c5
이강호 <tiger04176672@gmail.com>
2026-07-04 14:30:52 +0900

    [feat] 아이템데이터를 받을 ItemData 작성

---
+++ Assets/Script/ItemData.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item/New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Identity")]
+    [SerializeField] private string _itemName = "New Item";
+    [SerializeField] private Sprite _icon;
+    [SerializeField] private string _description = "";
+
+    [Header("Effect")]
+    [SerializeField] private int _healAmount = 20;
+
+    public string ItemName
+    {
+        get { return _itemName; }
+    }
+
+    public Sprite Icon
+    {
+        get { return _icon; }
+    }
+
+    public string Description
+    {
+        get { return _description; }
+    }
+
+    public int HealAmount
+    {
+        get { return _healAmount; }
+    }
+
+    public void Use(UnitHealth target)
+    {
+        if (target == null || target.IsDead) return;
+
+        target.Heal(_healAmount);
+    }
+}
